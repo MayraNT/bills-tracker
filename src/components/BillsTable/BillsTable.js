31f2f6a5366b73cbd2b79a425db7560b05ea5602
mayrant <mayrant2004@yahoo.com>
2021-11-30 16:46:51 -0600

    created bills table component and imported to dashboard

---
+++ src/components/BillsTable/BillsTable.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function BillsTable() {
+  return (
+    <div>
+      <p>Bills Table Component</p>
+      {/* <button>
+        <Link to="/">Logout</Link>
+      </button> */}
+    </div>
+  );
+}
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import BillsTable from "../BillsTable/BillsTable";
+
 export default function Dashboard() {
   return (
     <div>
-      <p>Dashboard Component</p>
+      <h1>Dashboard Component</h1>
+      <BillsTable />
       <button>
         <Link to="/">Logout</Link>
       </button>
