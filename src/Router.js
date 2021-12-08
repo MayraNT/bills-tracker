7e7b442dd393e1ad84976dc554ac530577781ba0
mayrant <mayrant2004@yahoo.com>
2021-12-08 02:01:23 -0600

    checkAuth function

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router";
+import cookie from "cookie";
 
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import Dashboard from "./components/Dashboard/Dashboard";
+
+const checkAuth = () => {
+  const cookies = cookie.parse(document.cookie);
+  console.log("cookies", cookies);
+
+  return cookies["loggedIn"] ? true : false;
+};
 
 const Router = () => {
   return (
@@ -13,7 +21,7 @@ const Router = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      {/* <Route path="/dashboard/user/:id" element={<Dashboard />} /> */}
+      {/* <Route path="/dashboard" element={checkAuth() ? <Dashboard /> : <Navigate to="/login" /> } /> */}
     </Routes>
   );
 };
