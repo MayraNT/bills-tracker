7080cef7f68c8ec4faa7fbb7831f4841eaebd732
mayrant <mayrant2004@yahoo.com>
2021-12-08 02:00:15 -0600

    bills table with data fetch

--- src/components/BillsTable/BillsTable.js
+++ src/components/BillsTable/BillsTable.js
@@ -1,13 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Checkbox,
+} from "@mui/material";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export default function BillsTable({ bills }) {
   return (
     <div>
-      <p>Bills Table Component</p>
-      <ul>
-        
-      </ul>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Due</TableCell>
+              <TableCell>Bill Name</TableCell>
+              <TableCell align="left">Amount</TableCell>
+              <TableCell align="center">Paid</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {bills.map((bill) => (
+              <TableRow
+                key={bill.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="center">{bill.due_day}</TableCell>
+                <TableCell component="th" scope="row">
+                  {bill.name}
+                </TableCell>
+                <TableCell align="left">{`$${bill.amount}`}</TableCell>
+                <TableCell align="center">
+                  <Checkbox {...label} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
