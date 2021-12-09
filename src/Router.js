ad17734c9b44ec0e7b446e457091cb47dc3ad692
mayrant <mayrant2004@yahoo.com>
2021-12-09 15:32:20 -0600

    added addbill component and protected routes

--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,7 @@ import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import Dashboard from "./components/Dashboard/Dashboard";
+import AddBill from "./components/AddBill/AddBill";
 
 const checkAuth = () => {
   const cookies = cookie.parse(document.cookie);
@@ -20,8 +21,14 @@ const Router = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      {/* <Route path="/dashboard" element={checkAuth() ? <Dashboard /> : <Navigate to="/login" /> } /> */}
+      <Route
+        path="/dashboard"
+        element={checkAuth() ? <Dashboard /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/add"
+        element={checkAuth() ? <AddBill /> : <Navigate to="/login" />}
+      />
     </Routes>
   );
 };
