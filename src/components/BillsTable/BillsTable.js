f5451107e690df55d7af5d83d14f3158c9384fb0
mayrant <mayrant2004@yahoo.com>
2021-12-12 22:08:28 -0600

    delete bill functionality

--- src/components/BillsTable/BillsTable.js
+++ src/components/BillsTable/BillsTable.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
+
 import {
   Table,
   TableBody,
@@ -10,7 +11,9 @@ import {
   TableRow,
   Paper,
   Checkbox,
+  IconButton,
 } from "@mui/material";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 import URL from "../../api";
 import styles from "./billsTable.module.css";
@@ -19,6 +22,7 @@ const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export default function BillsTable() {
   const [bills, setBills] = useState([]);
+  const [actions, setActions] = useState(false);
 
   const fetchBills = useCallback(async () => {
     const response = await axios(`${URL}/bills`);
@@ -29,16 +33,30 @@ export default function BillsTable() {
     fetchBills();
   }, [fetchBills]);
 
+  const deleteBill = (id) => {
+    console.log("this bill's id for DELETE:", id);
+    axios
+      .delete(`${URL}/bills/${id}`)
+      .then((response) => {
+        console.log(response);
+        const del = bills.filter((bill) => id !== bill.id);
+        setBills(del);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div>
       <TableContainer component={Paper}>
         <Table aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell align="center">Due</TableCell>
-              <TableCell>Bill</TableCell>
-              <TableCell align="left">Amount</TableCell>
               <TableCell align="center">Paid</TableCell>
+              <TableCell align="left">Due</TableCell>
+              <TableCell>Bill</TableCell>
+              <TableCell>Amount</TableCell>
+              {actions === true && <TableCell align="center">Delete</TableCell>}
+              <TableCell align="center">Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -47,13 +65,32 @@ export default function BillsTable() {
                 key={bill.id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
-                <TableCell align="center">{bill.due_day}</TableCell>
+                <TableCell align="center">
+                  <Checkbox {...label} style={{ color: 'secondary'}} />
+                </TableCell>
+                <TableCell align="left">{bill.due_day}</TableCell>
                 <TableCell component="th" scope="row">
                   {bill.name}
                 </TableCell>
-                <TableCell align="left">{`$${bill.amount}`}</TableCell>
+                <TableCell>{`$${bill.amount}`}</TableCell>
+                {actions === true && (
+                  <TableCell className={styles.delete}>
+                    <IconButton
+                      onClick={() => deleteBill(bill.id)}
+                      className={styles.delete}
+                      aria-label="delete"
+                    >
+                      <DeleteForeverIcon className={styles.delete}/>
+                    </IconButton>
+                  </TableCell>
+                )}
                 <TableCell align="center">
-                  <Checkbox {...label} />
+                  <button
+                    className={styles.actionsBtn}
+                    onClick={() => setActions(!actions)}
+                  >
+                    <span style={{ color: "#8FBC8F" }}><i class="fas fa-ellipsis-v"></i></span>
+                  </button>
                 </TableCell>
               </TableRow>
             ))}
