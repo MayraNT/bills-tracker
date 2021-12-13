93348037e9836ffe10b9c5d9820b7047925e91f2
mayrant <mayrant2004@yahoo.com>
2021-12-13 02:46:11 -0600

    dashboard styles

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -7,26 +7,48 @@ import styles from "./dashboard.module.css";
 
 const Dashboard = () => {
   console.log("logged in user's email--", localStorage.getItem("user_email"));
+
+  const date = new Date();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const currentMonth = months[date.getMonth()];
+  console.log(currentMonth);
+
   return (
     <div className={styles.container}>
       <div className={styles.sideDiv}>
         <h3 className={styles.greeting}>
           Hi, {localStorage.getItem("user_email")}!
-          <br />
-          Welcome to your Dashboard.
         </h3>
-        <Link to="/add">
-          <button>Add New Bill</button>
-        </Link>
+        <h4 className={styles.greeting2}>
+          <i class="far fa-grin"></i> Welcome to your {currentMonth} Dashboard.
+        </h4>
+        <div className={styles.btnsContainer}>
+          <button className={styles.dashBtn}
+            onClick={() => {
+              document.cookie = "loggedIn=";
+              window.location.replace("/");
+            }}
+          ><i class="fas fa-sign-out-alt"></i>
+            Logout 
+          </button>
+          <Link to="/add">
+            <button className={styles.dashBtn}><i class="fas fa-plus-circle"></i> Add New Bill</button>
+          </Link>
+        </div>
         <BanksFeature />
-        <button
-          onClick={() => {
-            document.cookie = "loggedIn=";
-            window.location.replace("/");
-          }}
-        >
-          Logout <i class="fas fa-sign-out-alt"></i>
-        </button>
       </div>
       <div className={styles.mainDiv}>
         <h2 className={styles.appName}>
